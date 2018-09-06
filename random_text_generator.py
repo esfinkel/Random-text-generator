0cb924045f71e61b7ebf76e62640bdbf349fc0c5
Elisabeth Finkel <32049677+esfinkel@users.noreply.github.com>
2018-09-06 15:49:52 -0400

    Add files via upload

---
+++ random_text_generator.py
@@ -0,0 +1,50 @@
+# Elisabeth Finkel
+# December 2017
+# Credits to http://www.physics.cornell.edu/~myers/teaching/ComputationalMethods/ComputerExercises/
+#
+
+import random
+import string
+from string import maketrans
+
+def read_file_into_word_list(filename):
+    inputFile = open(filename, 'r').read()
+    translator = maketrans(string.punctuation, ' '*len(string.punctuation))
+    fileWL = inputFile.translate(translator)
+    fileWL = fileWL.lower()
+    fileWL = fileWL.replace("\xe2\x80\x99","'").replace("xe2\x80\x9d","").replace("xe2\x80\x9c","")
+    fileWL = fileWL.split()
+    return fileWL
+
+
+def make_prefix_dictionary(filename):
+    words = read_file_into_word_list(filename)
+    prefix = {}
+    for i in range(0, len(words)-2):
+        if (words[i],words[i+1]) not in prefix:
+            prefix[(words[i],words[i+1])]=[]
+        prefix[(words[i],words[i+1])].append(words[i+2])
+    #print prefix
+    return prefix
+
+def make_random_text(filename, num_words=100):
+    """
+    Input: file path + name (string). Return random_text with num_words (default 100) words.
+    """
+    prefix = make_prefix_dictionary(str(filename))
+    current_pair = random.choice(prefix.keys())
+    random_text = current_pair[0] + ' ' + current_pair[1]
+    for i in range(num_words-2):
+        if current_pair not in prefix:
+            break
+            #r = random.choice(prefix.keys())
+            #random_text = random_text + " " + r[1]
+        else:
+            r = random.choice(prefix[current_pair])
+            random_text = random_text + " " + r
+            rand_list = random_text.split()
+            current_pair = (rand_list[i+1],rand_list[i+2])
+        #print current_pair
+    return random_text
+
+        

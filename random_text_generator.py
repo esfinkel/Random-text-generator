550f6deb880602419ccdee618a3a38fd0b035c49
Elisabeth Finkel <32049677+esfinkel@users.noreply.github.com>
2018-10-09 10:57:19 -0400

    debugged

--- random_text_generator.py
+++ random_text_generator.py
@@ -1,15 +1,14 @@
 # Elisabeth Finkel
 # December 2017
-# Credits to http://www.physics.cornell.edu/~myers/teaching/ComputationalMethods/ComputerExercises/
+# Credits for idea to http://www.physics.cornell.edu/~myers/teaching/ComputationalMethods/ComputerExercises/
 #
 
 import random
 import string
-from string import maketrans
 
 def read_file_into_word_list(filename):
     inputFile = open(filename, 'r').read()
-    translator = maketrans(string.punctuation, ' '*len(string.punctuation))
+    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
     fileWL = inputFile.translate(translator)
     fileWL = fileWL.lower()
     fileWL = fileWL.replace("\xe2\x80\x99","'").replace("xe2\x80\x9d","").replace("xe2\x80\x9c","")
@@ -32,7 +31,7 @@ def make_random_text(filename, num_words=100):
     Input: file path + name (string). Return random_text with num_words (default 100) words.
     """
     prefix = make_prefix_dictionary(str(filename))
-    current_pair = random.choice(prefix.keys())
+    current_pair = random.choice(list(prefix.keys()))
     random_text = current_pair[0] + ' ' + current_pair[1]
     for i in range(num_words-2):
         if current_pair not in prefix:
@@ -47,4 +46,6 @@ def make_random_text(filename, num_words=100):
         #print current_pair
     return random_text
 
-        
+if __name__ == '__main__':
+    from sys import argv
+    print(make_random_text(argv[0]))
